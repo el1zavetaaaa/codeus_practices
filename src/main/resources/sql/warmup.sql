ad38bf916ea0435d820a4ca2abfa27ce6a23ce9b
yelyzavetalubenets <el1111zavetaaaaa@gmail.com>
2025-01-02 20:45:05 +0200

    practicing sql - day 1

--- src/main/resources/sql/warmup.sql
+++ src/main/resources/sql/warmup.sql
@@ -7,16 +7,15 @@
 -- #0. EASY.
 -- Insert into table 'students' your name (or nick) and your age.
 -- Name length must be 3 or more letters and Age > 15
-INSERT INTO students (name, age)
-VALUES ('YourName', 0);
+insert into students (name, age)
+values ('Yelyzaveta Lubenets', 0);
 
 -- ------------------------------------------------------------------------------------------------
 -- #1. EASY.
 -- Calculate average age of users [double/numeric] (in any option)
 -- Result: one field  -> "average_age"
-SELECT *
--- TODO: Write your solution here
-FROM users as t1;
+select avg(age) as average_age
+from users as t1;
 
 -- ------------------------------------------------------------------------------------------------
 -- #2. EASY-MEDIUM-HARD (depends on the solution path you choose)
@@ -25,9 +24,15 @@ FROM users as t1;
 
 -- Note: Use CURRENT_DATE for getting current date in Postgres DB.
 
-SELECT *
--- TODO: Write your solution here
-FROM users as t1;
+-- easy
+select t1.id,
+       sum ((a.amount * r.rate) / 100.0) as uah_eq_acc
+from users as t1
+join public.accounts a on t1.id = a.user_id
+join nbu_rates r on a.currency = r.ccy
+and r.ccy_date = '2024-12-05'
+group by t1.id
+order by t1.id;
 
 -- ------------------------------------------------------------------------------------------------
 -- #2a. EASY-MEDIUM-HARD (depends on the solution path you choose)
@@ -100,3 +105,45 @@ FROM users as t1;
 SELECT *
 -- TODO: Write your solution here
 FROM students;
+
+create table products (
+    id bigserial primary key,
+    name text not null,
+    price decimal (8,2),
+    created_at timestamp default now()
+);
+
+insert into products(name, price)
+values
+    ('banana', 14),
+    ('banana', 14),
+    ('apple', 12),
+    ('orange', 12),
+    ('pineapple', 12);
+
+-- find duplicate names
+
+-- v1 ( group by + having )
+select name
+from products
+group by name
+having count(name) > 1;
+
+-- v2 ( self join )
+select p1.name
+from products p1
+join public.products p2 on p1.id = p2.id
+group by p1.name
+having count(p1.name) > 1;
+
+-- v3 ( window function )
+with name_counts as
+    ( select name,
+             count(*) over (partition by name) as name_count
+      from products)
+
+select distinct name
+from name_counts
+where name_count > 1;
+
+
